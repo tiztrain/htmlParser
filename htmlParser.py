5f0140ba36640ce987cef19b1eaf0f8db6112fee
tiztrain <adamtolo20@hotmail.com>
2018-10-14 20:51:45 +1000

    successfully been able to find the links for web services

--- htmlParser.py
+++ htmlParser.py
@@ -8,28 +8,56 @@ print(home_directory)
 
 # example of HTMLParser class
 class MyHTMLParser(HTMLParser):
+
+    # this function will extract the web map service
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+        print("Encountered a start tag     :", tag)
+        for attr in attrs:
+            print("                         attr:", attr)
+            print("                         attr1:", attr[0])
+            print("                         attr2:", attr[1])
+            if tag == "a" and attr[0] == 'href':
+                print("**************************************************************************")
+
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+        print("Encountered an end tag      :", tag)
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        print("Encountered some data       :", data)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Encountered a joined tag    :", tag)
+
+    # NON PICKED UP
+    # def handle_entityref(self, name):
+    #     print("Encountered entity reference:", name)
+    #
+    # def unknown_decl(self, data):
+    #     print("Unrecognised declaration    :", data)
+
+
+def testData(input):
+    # test data options
+    # result = '<html><head><title>Test</title></head>'' \
+    #         ''<body><h1>Parse me!</h1></body></html>'
+    result = input
+    return result
+
 
 
 def main():
 
-    # create HTMLParser object and test class with test data
-    parser = MyHTMLParser()
-    parser.feed('<html><head><title>Test</title></head>'
-                '<body><h1>Parse me!</h1></body></html>')
-
     # read open file and read
     f = open(home_directory + r'\_in\aus_geoscience_services.html', 'r')
-    print(f)
     contents = f.read()
-    print(contents)
+    # # print(contents)
+    # testData(contents)
+
+    # create HTMLParser object
+    parser = MyHTMLParser()
+    parser.feed(testData(contents))
+
 
 
 if __name__== '__main__':
