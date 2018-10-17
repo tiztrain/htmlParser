729a9201136de0c718e49dab73115ed3f7680cb2
tiztrain <adamtolo20@hotmail.com>
2018-10-17 18:33:54 +1000

    -put all test data into one test function -filtered out the weblinks that were not web services -added NSW spatial dataset to the test options -change the source of the data to directly to the URL
    -can't figure out how to get the layer title yet

--- htmlParser.py
+++ htmlParser.py
@@ -1,4 +1,5 @@
 from html.parser import HTMLParser
+import urllib.request
 import os
 
 # setup project home folder
@@ -6,26 +7,29 @@ os.chdir(r'D:\Users\tizalien\PycharmProjects\htmlParser')
 home_directory = os.getcwd()
 print(home_directory)
 
+
 # example of HTMLParser class
 class MyHTMLParser(HTMLParser):
 
     # this function will extract the the details I require
     def handle_starttag(self, tag, attrs):
-        # print statements to be able to break down the html code
-        """
-        #print the tag
-        print("Encountered a start tag     :", tag)
-        print the attributes in the tag
-        """
 
+        # # print statements to be able to break down the html code
+        # """print the tag"""
+        # print("Encountered a start tag     :", tag)
 
+        """print the attributes in the tag"""
         for attr in attrs:
             # for testing what attributes are included in the tag and testing splitting them up
-            """
-            print("                         attr:", attr)
-            print("                         attr1:", attr[0])
-            print("                         attr2:", attr[1])
-            """
+
+            # print("                         attr:", attr)
+        #     print("                         attr1:", attr[0])
+        #     print("                         attr2:", attr[1])
+
+            # """ THIS CODE EXTRACTS THE TITLE FOR THE WEBSERVICE"""
+            # # TODO import the data tag into this function somehow
+            # if tag == 'div' and attr[1] == 'tileTitle':
+            #     print(self.handle_data(data))
 
             # THIS CODE EXTRACTS THE WEB SERVICES LINKS
             # lines with the tag 'a' and the attribute 'href' are the web service links
@@ -39,24 +43,26 @@ class MyHTMLParser(HTMLParser):
                     if 'server' in ws_link.casefold():
                         print(ws_link)
 
-    # not needed yet but maybe for extracting titles and other attributes
-"""
-    def handle_endtag(self, tag):
-        print("Encountered an end tag      :", tag)
+    """ not needed yet but maybe for extracting titles and other attributes"""
 
-    def handle_data(self, data):
-        print("Encountered some data       :", data)
+    # def handle_endtag(self, tag):
+    #     print("Encountered an end tag      :", tag)
+    #
+    # def handle_data(self, data):
+    #     print("Encountered some data       :", data)
 
-    def handle_startendtag(self, tag, attrs):
-        print("Encountered a joined tag    :", tag)
 
-    NONE PICKED UP
-    def handle_entityref(self, name):
-        print("Encountered entity reference:", name)
+    #
+    # def handle_startendtag(self, tag, attrs):
+    #     print("Encountered a joined tag    :", tag)
+    #
+    # NONE PICKED UP
+    # def handle_entityref(self, name):
+    #     print("Encountered entity reference:", name)
+    #
+    # def unknown_decl(self, data):
+    #     print("Unrecognised declaration    :", data)
 
-    def unknown_decl(self, data):
-        print("Unrecognised declaration    :", data)
-"""
 
 
 
@@ -67,10 +73,11 @@ def testData():
     # result = '<html><head><title>Test</title></head>'' \
     #         ''<body><h1>Parse me!</h1></body></html>'
 
-    # CHOOSE ONE real html test
-    # f = open(home_directory + r'\_in\aus_geoscience_services.html', 'r')
-    f = open(home_directory + r'\_in\spatialservices_finance_nsw_gov_au.html', 'r')
-    result = f.read()
+    # CHOOSE ONE real online html test
+    f = urllib.request.urlopen('http://services.ga.gov.au/')
+    # f = urllib.request.urlopen(
+    #     'http://spatialservices.finance.nsw.gov.au/mapping_and_imagery/lpi_web_services/access_lpi_web_services')
+    result = str(f.read())
 
     return result
 
